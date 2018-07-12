8c1ab715a850bd132e58c4e6a73d0eba50a51c9c
Maximilian Fackler <derultralord.mf@web.de>
2018-07-12 20:04:39 +0200

    Bugfix: grid view cards not wrapping to new line

--- src/renderer/CardList.vue
+++ src/renderer/CardList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div >
+    <div class="d-flex flex-wrap">
         <Card v-for="c in cards" v-bind:data="c" v-bind:key="c.id" :card=c :state=state v-bind:class="[state.settings.isGridActive ? 'd-flex flex-row flex-wrap' : 'list-group-item']"></Card>
     </div>
 </template>
